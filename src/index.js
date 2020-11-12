250298f1fee8f7a284557ca190cb54249382735d
Mark Tripney <hello@marktripney.com>
2020-11-12 11:03:00 +0100

    Add basic Card component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.css';
+import notes from './img/notes-icon.png';
+import deadline from './img/deadline-icon.png';
+import assignee from './img/assignee-icon.png';
 
-function Placeholder() {
-  return <h1>React Kanban</h1>;
+function Card() {
+  return (
+    <div className="card">
+      <p>Task heading</p>
+      <div className="card-icons">
+        <span className="deadline-icon">
+          <img src={deadline} alt="Deadline icon" />
+        </span>
+        <span className="notes-icon">
+          <img src={notes} alt="Notes icon" />
+        </span>
+        <span className="assignee-img">
+          <img src={assignee} alt="Assignee icon" />
+        </span>
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(<Placeholder />, document.querySelector('#root'));
+ReactDOM.render(<Card />, document.querySelector('#root'));
